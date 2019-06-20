204b48ed4074b1044d3e4d3dfc7e894d1a26b90e
Luiz Junior <inf.gonzaga@gmail.com>
2019-06-20 18:17:03 -0300

    Calendario DataServico

--- app/src/main/java/br/com/app_barber/CalendarioActivity.java
+++ app/src/main/java/br/com/app_barber/CalendarioActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CalendarView;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -12,11 +13,16 @@ import com.google.firebase.database.FirebaseDatabase;
 public class CalendarioActivity extends AppCompatActivity {
 
     private Button btnSalvarData;
+    private CalendarView cvDataServico;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendario);
+
+        cvDataServico = (CalendarView) findViewById(R.id.cvDataServico); // get the reference of CalendarView
+        long dataservico = cvDataServico.getDate(); // get selected date in milliseconds
+
 
         btnSalvarData = (Button) findViewById(R.id.btnSalvarData);
 
